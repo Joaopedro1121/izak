2a2f60ac9bc0988f1b8b0a3e25b3709174155a71
Joaopedro1121 <Joao.grin@escola.pr.gov.br>
2025-06-04 10:29:50 -0300

    55

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'futebol'
+    'qual o maior goleador do mundo'
+    'cristiano ronaldo'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'basquete',
+    'Qual o maior jogador de basquete?',
+    'Michael Jordan'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'jogos de gta rp',
+    'qual o maior influencer de gta rp?',
+    'Paulinho O Loko'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'tenis de mesa',
+    'qual o melhor jogador de tenis de mesa?',
+    'Fan Zhendong'
 )
